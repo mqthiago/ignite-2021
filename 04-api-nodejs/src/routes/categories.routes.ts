127324e1021debced18f0bfa62d2fff0d4f82cd1
Thiago <thiago.poison@gmail.com>
2022-08-07 18:23:30 -0300

    Init api with nodeJS

---
+++ 04-api-nodejs/src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+export const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return response.status(409).json({ error: "Category Already exists!" });
+  }
+
+  categoriesRepository.create({ name, description });
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
